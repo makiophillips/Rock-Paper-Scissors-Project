f02a07ed70622f167c599f0fb6c7dae3945473c7
Makio Phillips <makio.phillips@yahoo.com>
2022-08-30 14:22:31 -0500

    moved scripts around for readability and added js

--- myscripts.js
+++ myscripts.js
@@ -1,66 +1,64 @@
-const playerSelection = getUserChoice();
-const computerSelection = getComputerChoice();
-let playerScore = 0;
-let computerScore = 0;
-let playerWinRound = "Player wins this round!"
-let computerWinRound = "Computer wins this round!"
-let draw = "Draw!"
-let playerWin = "Player wins the game! Congratulations!"
-let computerWin = "Computer wins the game! Congratulations!"
-
 function getUserChoice(userInput) {
-    let userChoice = userInput;
-      if(userChoice === "rock" || userChoice === "paper" || userChoice === "scissors") {
-        return userChoice;
+    let userChoice = userInput.toLowerCase();
+      if (userChoice === "rock" || userChoice === "paper" || userChoice === "scissors") {
+        return userInput;
       }
-      else {
-        return "That didn't quite work.";
-    }
+    console.log(userInput)
   }
 
-  function getComputerChoice() {
+function getComputerChoice() {
     let computerChoice = Math.floor(Math.random()*3);
-        if(computerChoice === 0){
+        if (computerChoice === 0) {
         return "rock";
-    }
-        else if(computerChoice === 1)  {
-        return "scissors";
-    }
-        else if(computerChoice === 2) { 
-        return "paper";
-    }
-        else {
-            return "Input not valid.";
         }
-  }
+        else if (computerChoice === 1) {
+        return "paper";
+        }
+        else if(computerChoice === 2) {
+        return "scissors";
+        }
+        console.log(computerChoice)
+}
 
-
-function determineWinner() {
-    if (userChoice === getComputerChoice) {
-        return "Tie!";
+function playRound(playerSelection, computerSelection){
+    let userChoice = userInput;
+    let computerChoice = getComputerChoice();
+   console.log('getComputerChoice:' + computerChoice);
+    if (userChoice === computerChoice) {
+        return draw;
     }
-    else if (userChoice === 'scissors' && getComputerChoice === 'paper') {
+    else if (userChoice === 'scissors' && computerChoice === 'paper') {
         return ('Scissors cut paper.' + playerWinRound);
     }
-        else if (getComputerChoice === 'rock') {
+        else if (computerChoice === 'rock') {
             return ('Rock breaks scissors.' + computerWinRound) ;
         }
-    else if (userChoice === 'paper' && getComputerChoice === 'rock') {
+    else if (userChoice === 'paper' && computerChoice === 'rock') {
         return ('Paper covers Rock.' + playerWinRound); 
     }
-        else if (getComputerChoice === 'scissors') {
+        else if (computerChoice === 'scissors') {
             return ('Scissors cut paper.'  + computerWinRound);
         }
-    else if (userChoice === 'rock' && getComputerChoice === 'scissors') {
+    else if (userChoice === 'rock' && computerChoice === 'scissors') {
         return ('Rock breaks scissors.' + playerWinRound);
-        }
-        else if (getComputerChoice === 'paper') {
+    }
+        else if (computerChoice === 'paper') {
             return ('Paper covers rock.'  + computerWinRound);
         }
             else {
-                return "That's not right!"
+                return "That's not right!";
             }
 }
 
+ function game() {
+  let (round = playRound);
+  for (let i = 0; i < 5; i++) ; {
+    if ((i > 0) && (i != 0) && (i <= 5));
+    return ("Round" + i)
+  }
+}
+
+const playerSelection = getUserChoice
+const computerSelection = getComputerChoice
 
 
