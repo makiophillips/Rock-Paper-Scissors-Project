c401a7815300937904e6e7c70eee44e27c9aaa50
Makio Phillips <makio.phillips@yahoo.com>
2022-08-29 22:18:24 -0500

    added functions to get a score

--- myscripts.js
+++ myscripts.js
@@ -1,6 +1,16 @@
+const playerSelection = getUserChoice();
+const computerSelection = getComputerChoice();
+let playerScore = 0;
+let computerScore = 0;
+let playerWinRound = "Player wins this round!"
+let computerWinRound = "Computer wins this round!"
+let draw = "Draw!"
+let playerWin = "Player wins the game! Congratulations!"
+let computerWin = "Computer wins the game! Congratulations!"
+
 function getUserChoice(userInput) {
     let userChoice = userInput;
-      if(userChoice === "Rock" || userChoice === "Paper" || userChoice === "Scissors") {
+      if(userChoice === "rock" || userChoice === "paper" || userChoice === "scissors") {
         return userChoice;
       }
       else {
@@ -10,17 +20,47 @@ function getUserChoice(userInput) {
 
   function getComputerChoice() {
     let computerChoice = Math.floor(Math.random()*3);
-       if(computerChoice === 0){
+        if(computerChoice === 0){
         return "rock";
     }
-      else if(computerChoice === 1)  {
+        else if(computerChoice === 1)  {
         return "scissors";
     }
-      else if(computerChoice === 2) { 
+        else if(computerChoice === 2) { 
         return "paper";
     }
-          else {
-            return "Input not valid.";}
+        else {
+            return "Input not valid.";
+        }
   }
 
-  
+
+function determineWinner() {
+    if (userChoice === getComputerChoice) {
+        return "Tie!";
+    }
+    else if (userChoice === 'scissors' && getComputerChoice === 'paper') {
+        return ('Scissors cut paper.' + playerWinRound);
+    }
+        else if (getComputerChoice === 'rock') {
+            return ('Rock breaks scissors.' + computerWinRound) ;
+        }
+    else if (userChoice === 'paper' && getComputerChoice === 'rock') {
+        return ('Paper covers Rock.' + playerWinRound); 
+    }
+        else if (getComputerChoice === 'scissors') {
+            return ('Scissors cut paper.'  + computerWinRound);
+        }
+    else if (userChoice === 'rock' && getComputerChoice === 'scissors') {
+        return ('Rock breaks scissors.' + playerWinRound);
+        }
+        else if (getComputerChoice === 'paper') {
+            return ('Paper covers rock.'  + computerWinRound);
+        }
+            else {
+                return "That's not right!"
+            }
+}
+
+
+
