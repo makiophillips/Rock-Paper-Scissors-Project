a44097321a44037b85bc308521df2694a0996bd9
Makio Phillips <makio.phillips@yahoo.com>
2022-08-29 21:29:31 -0500

    added function for computer

--- myscripts.js
+++ myscripts.js
@@ -1,9 +1,26 @@
-function getUserChoice(userInput){
+function getUserChoice(userInput) {
     let userChoice = userInput;
-      if(userChoice === "Rock" || userChoice === "Paper" || userChoice === "Scissors"){
+      if(userChoice === "Rock" || userChoice === "Paper" || userChoice === "Scissors") {
         return userChoice;
       }
       else {
         return "That didn't quite work.";
     }
   }
+
+  function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random()*3);
+       if(computerChoice === 0){
+        return "rock";
+    }
+      else if(computerChoice === 1)  {
+        return "scissors";
+    }
+      else if(computerChoice === 2) { 
+        return "paper";
+    }
+          else {
+            return "Input not valid.";}
+  }
+
+  
